0a2aed73c5cab03cfa94ec9ea1a1155d5c4c5d1c
Milena Govor <milena.govorr@gmail.com>
2023-12-04 01:41:51 +0300

    add function addition

---
+++ function.cpp
@@ -0,0 +1,6 @@
+// function.cpp
+#include "function.h"
+
+double Function::add(double a, double b) {
+    return a + b;
+}
---
+++ main.cpp
@@ -0,0 +1,27 @@
+// main.cpp
+#include <iostream>
+#include "function.h"
+
+using namespace std;
+
+int main() {
+    double a, b;
+    char operation;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Choose operation (+, -, *, /): ";
+    cin >> operation;
+
+    switch (operation) {
+    case '+':
+        cout << "Result: " << Function::add(a, b) << endl;
+        break;
+        default:
+            cout << "Incorrect operation!" << endl;
+            break;
+   }
+
+    return 0;
+}
